731f19da115d4c83a9f748aa46f38c148b20afa0
Kelly Bolding <kbolding@princeton.edu>
2016-11-03 19:54:41 -0400

    Added file path prompts.

--- add-extensions.sh
+++ add-extensions.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-#This script finds individual files anywhere within a directory (here, within "cdo") and renames them by appending the appropriate file extension based on a lookup csv file.
+#This script finds individual files anywhere within a directory and renames them by appending the appropriate file extension based on a lookup csv file.
 
-#Input file named "file" should be a simple csv file with file names in column 1 ($f1) and extensions to be appended in column 2 ($f). This input file can be created from DROID ext-mismatch report.
+#Input file named "file" should be a simple csv file with filenames in column 1 ($f1) and extensions to be appended in column 2 ($f2). This input file can be created from DROID ext-mismatch report.
 
-#To call script, enter the following into the terminal: bash ./add-extensions.sh
+clear
+
+echo -e "Enter the full path to the top-level folder you want to search/add extensions."
+read cdo
+
+echo -e "Enter the full path to the folder where you want to store the log file."
+read log
 
 while IFS=, read f1 f2
 do
-	find ./cdo -type f -name "$f1" -execdir mv -v "$f1" "$f1".$f2 \;
-done < file > log.txt
+	find "$cdo" -type f -name "$f1" -execdir mv -v "$f1" "$f1".$f2 \;
+done < file > "$log"/log.txt
