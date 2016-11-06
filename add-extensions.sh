eee3ccda12aeec5dbd79405f471ef14ad856b63e
Kelly Bolding <kbolding@princeton.edu>
2016-11-06 15:44:48 -0500

    Fixed dup filename bug, edited input specs
    Changed input specs/removed find command; turned input file into variable

--- add-extensions.sh
+++ add-extensions.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-#This script finds individual files anywhere within a directory and renames them by appending the appropriate file extension based on a lookup csv file.
+#This script bulk renames files by appending the appropriate file extension based on a file path and extension from a csv input file.
 
-#Input file named "file" should be a simple csv file with filenames in column 1 ($f1) and extensions to be appended in column 2 ($f2). This input file can be created from DROID ext-mismatch report.
+#Input csv file ($input) should contain the full file path in column 1 ($f1) and extension to be appended in column 2 ($f2). This input file can be created from DROID ext-mismatch report.
+
+#To call script, enter the following into the terminal: bash ./add-extensions.sh
 
 clear
 
-echo -e "Enter the full path to the top-level folder you want to search/add extensions."
-read cdo
+echo -e "Enter the full path of your input file."
+read input
 
-echo -e "Enter the full path to the folder where you want to store the log file."
+echo -e "Enter the full path to the location where you want to store the log file."
 read log
 
 while IFS=, read f1 f2
 do
-	find "$cdo" -type f -name "$f1" -execdir mv -v "$f1" "$f1".$f2 \;
-done < file > "$log"/log.txt
+	mv -v "$f1" "$f1".$f2
+done < "$input" > "$log"/log.txt
