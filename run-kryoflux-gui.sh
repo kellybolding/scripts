7117b1a4516c655f02a6a7ddbf7d70bf2211f3bf
Kelly Bolding <kbolding@princeton.edu>
2017-09-26 11:52:56 -0400

    Added comment.

--- run-kryoflux-gui.sh
+++ run-kryoflux-gui.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #runs kryoflux gui by invoking java
+#to reuse: edit file path to match your installation location
 
 ln -s /home/bcadmin/Additional_Software/kryoflux_2.6_linux/dtc cddtc
 
