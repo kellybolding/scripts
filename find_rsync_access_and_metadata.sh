9cab84c80531c07b34465819854bfa467e744ce4
Kelly Bolding <kbolding@princeton.edu>
2018-06-26 13:03:25 -0400

    added .csv as metadata type captured

--- find_rsync_access_and_metadata.sh
+++ find_rsync_access_and_metadata.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#purpose: copying metadata (.txt and .xml) and access (.mp3 and .mp4) files ONLY from vendor drives containing digitized audiovisual materials to desired folder
+#purpose: copying metadata (.txt, .csv, and .xml) and access (.mp3 and .mp4) files ONLY from vendor drives containing digitized audiovisual materials to desired folder
 #note: you may see permissions errors in the terminal, but ignore because they are for the system files on the vendor drive itself
 #repurpose: easy to edit file extensions you want to find
 #citation: commands largely stolen from https://dd388.github.io/crals/#find_rsync (though any errors introduced are courtesy of KB)
@@ -13,9 +13,9 @@ read sourcedrive
 echo "Enter the full path to the destination where you want to copy files (do NOT include trailing slash)"
 read destination
 
-#finds and copies metadata files (.txt and .xml) from source drive and puts them in a newly created folder called "metadata" within destination folder
+#finds and copies metadata files (.txt, .csv, and .xml) from source drive and puts them in a newly created folder called "metadata" within destination folder
 
-find "$sourcedrive" \( -iname "*.xml" -o -iname "*.txt" \) -exec rsync {} "$destination"/metadata/ \;
+find "$sourcedrive" \( -iname "*.xml" -o -iname "*.txt" -o -iname "*.csv" \) -exec rsync {} "$destination"/metadata/ \;
 
 #finds and copies access files (.mp4 and .mp3) and puts them in a newly created folder called "access" within the destination folder
 
